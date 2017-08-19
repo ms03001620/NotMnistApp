3cd41fffb313e3582a8c25eb794ba3f367e68f43
Mark <mazhenjin@sina.com>
2017-08-19 17:09:31 +0800

    指定nodpi文件后可以正确使用pb 并读取正确的softmax

--- app/src/main/java/com/example/mark/loadtensorflowmodeltest/MainActivity.java
+++ app/src/main/java/com/example/mark/loadtensorflowmodeltest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mark.loadtensorflowmodeltest;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
@@ -21,6 +22,9 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String YOLO_MODEL_FILE = "file:///android_asset/tiny-yolo-voc.pb";
+
+    private static final String NOTMNIST_MODEL_FILE = "file:///android_asset/not-mnist-a-j-tf1.2.pb";
+
     private static final int YOLO_INPUT_SIZE = 416;
     private static final String YOLO_INPUT_NAME = "input";
     private static final String YOLO_OUTPUT_NAMES = "output";
@@ -32,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
     TextView mTextView;
     InfoImageView image;
-    InfoImageView imageCopped;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mTextView = (TextView)findViewById(R.id.text);
         image = (InfoImageView)findViewById(R.id.image);
-        imageCopped = (InfoImageView)findViewById(R.id.image_crop);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -49,59 +51,29 @@ public class MainActivity extends AppCompatActivity {
                 long start = System.currentTimeMillis();
 
 
-                Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
 
-                Bitmap cropped = getCroppedBitmap(bitmap);
 
-                imageCopped.setImageBitmap(cropped);
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.a0);
 
-                final List<Classifier.Recognition> results = detector.recognizeImage(cropped);
+
+                detector.recognizeImage(bitmap);
 
                 mTextView.setText("total pass:"+(System.currentTimeMillis()-start));
 
                 Log.v("MainAcivity", "total pass:"+(System.currentTimeMillis()-start));
 
-
-                imageCopped.setResult(results);
             }
         });
 
         detector = TensorFlowYoloDetector.create(
                         getAssets(),
-                        YOLO_MODEL_FILE,
-                        YOLO_INPUT_SIZE,
-                        YOLO_INPUT_NAME,
-                        YOLO_OUTPUT_NAMES,
-                        YOLO_BLOCK_SIZE);
+                NOTMNIST_MODEL_FILE,
+                        28,
+                        "input:0",
+                        "out_softmax:0");
 
     }
 
-    private Bitmap getCroppedBitmap(Bitmap source){
-        long start = System.currentTimeMillis();
-        int previewWidth = source.getWidth();
-        int previewHeight = source.getHeight();
-        int[] rgbBytes = new int[previewWidth * previewHeight];
 
-
-        Bitmap rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Bitmap.Config.ARGB_8888);
-        Bitmap croppedBitmap = Bitmap.createBitmap(CROP_SIZE, CROP_SIZE, Bitmap.Config.ARGB_8888);
-
-        Matrix frameToCropTransform = ImageUtils.getTransformationMatrix(previewWidth, previewHeight, CROP_SIZE, CROP_SIZE, 0, true);
-
-        Matrix cropToFrameTransform = new Matrix();
-        frameToCropTransform.invert(cropToFrameTransform);
-
-        byte[][] yuvBytes = new byte[3][];
-
-
-        rgbFrameBitmap.setPixels(rgbBytes, 0, previewWidth, 0, 0, previewWidth, previewHeight);
-
-        final Canvas canvas = new Canvas(croppedBitmap);
-        canvas.drawBitmap(source, frameToCropTransform, null);
-
-        Log.v("MainAcivity", "getCroppedBitmap pass:"+(System.currentTimeMillis()-start));
-
-        return croppedBitmap;
-    }
 
 }
--- app/src/main/java/com/example/mark/loadtensorflowmodeltest/TensorFlowYoloDetector.java
+++ app/src/main/java/com/example/mark/loadtensorflowmodeltest/TensorFlowYoloDetector.java
@@ -1,75 +1,14 @@
-/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
 package com.example.mark.loadtensorflowmodeltest;
 
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
-import android.graphics.RectF;
 
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
-import java.util.ArrayList;
-import java.util.Comparator;
+
 import java.util.List;
-import java.util.PriorityQueue;
 
-/**
- * An object detector that uses TF and a YOLO model to detect objects.
- */
+
 public class TensorFlowYoloDetector implements Classifier {
-
-    // Only return this many results with at least this confidence.
-    private static final int MAX_RESULTS = 5;
-
-    private static final int NUM_CLASSES = 20;
-
-    private static final int NUM_BOXES_PER_BLOCK = 5;
-
-    // TODO(andrewharp): allow loading anchors and classes
-    // from files.
-    private static final double[] ANCHORS = {
-            1.08, 1.19,
-            3.42, 4.41,
-            6.63, 11.38,
-            9.42, 5.11,
-            16.62, 10.52
-    };
-
-    private static final String[] LABELS = {
-            "aeroplane",
-            "bicycle",
-            "bird",
-            "boat",
-            "bottle",
-            "bus",
-            "car",
-            "cat",
-            "chair",
-            "cow",
-            "diningtable",
-            "dog",
-            "horse",
-            "motorbike",
-            "person",
-            "pottedplant",
-            "sheep",
-            "sofa",
-            "train",
-            "tvmonitor"
-    };
-
     // Config values.
     private String inputName;
     private int inputSize;
@@ -79,7 +18,6 @@ public class TensorFlowYoloDetector implements Classifier {
     private float[] floatValues;
     private String[] outputNames;
 
-    private int blockSize;
 
     private boolean logStats = false;
 
@@ -93,8 +31,7 @@ public class TensorFlowYoloDetector implements Classifier {
             final String modelFilename,
             final int inputSize,
             final String inputName,
-            final String outputName,
-            final int blockSize) {
+            final String outputName) {
         TensorFlowYoloDetector d = new TensorFlowYoloDetector();
         d.inputName = inputName;
         d.inputSize = inputSize;
@@ -102,8 +39,8 @@ public class TensorFlowYoloDetector implements Classifier {
         // Pre-allocate buffers.
         d.outputNames = outputName.split(",");
         d.intValues = new int[inputSize * inputSize];
-        d.floatValues = new float[inputSize * inputSize * 3];
-        d.blockSize = blockSize;
+        d.floatValues = new float[inputSize * inputSize];
+
 
         d.inferenceInterface = new TensorFlowInferenceInterface(assetManager, modelFilename);
 
@@ -113,116 +50,35 @@ public class TensorFlowYoloDetector implements Classifier {
     private TensorFlowYoloDetector() {
     }
 
-    private float expit(final float x) {
-        return (float) (1. / (1. + Math.exp(-x)));
-    }
-
-    private void softmax(final float[] vals) {
-        float max = Float.NEGATIVE_INFINITY;
-        for (final float val : vals) {
-            max = Math.max(max, val);
-        }
-        float sum = 0.0f;
-        for (int i = 0; i < vals.length; ++i) {
-            vals[i] = (float) Math.exp(vals[i] - max);
-            sum += vals[i];
-        }
-        for (int i = 0; i < vals.length; ++i) {
-            vals[i] = vals[i] / sum;
-        }
-    }
 
     @Override
     public List<Recognition> recognizeImage(final Bitmap bitmap) {
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+
+
         // Preprocess the image data from 0-255 int to normalized float based
         // on the provided parameters.
         bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
 
-        for (int i = 0; i < intValues.length; ++i) {
-            floatValues[i * 3 + 0] = ((intValues[i] >> 16) & 0xFF) / 255.0f;
-            floatValues[i * 3 + 1] = ((intValues[i] >> 8) & 0xFF) / 255.0f;
-            floatValues[i * 3 + 2] = (intValues[i] & 0xFF) / 255.0f;
+        for(int i = 0;i<intValues.length;i++){
+            int a = intValues[i];
+            floatValues[i] = a;
         }
 
-        // Copy the input data into TensorFlow.
-        inferenceInterface.feed(inputName, floatValues, 1, inputSize, inputSize, 3);
+        inferenceInterface.feed(inputName, floatValues, 1, inputSize, inputSize, 1);
 
 
         // Run the inference call.
         inferenceInterface.run(outputNames, logStats);
 
 
-        // Copy the output Tensor back into the output array.
-        final int gridWidth = bitmap.getWidth() / blockSize;
-        final int gridHeight = bitmap.getHeight() / blockSize;
-        final float[] output = new float[gridWidth * gridHeight * (NUM_CLASSES + 5) * NUM_BOXES_PER_BLOCK];
+
+        final float[] output = new float[10];
         inferenceInterface.fetch(outputNames[0], output);
 
-        // Find the best detections.
-        final PriorityQueue<Recognition> pq =
-                new PriorityQueue<Recognition>(
-                        1,
-                        new Comparator<Recognition>() {
-                            @Override
-                            public int compare(final Recognition lhs, final Recognition rhs) {
-                                // Intentionally reversed to put high confidence at the head of the queue.
-                                return Float.compare(rhs.getConfidence(), lhs.getConfidence());
-                            }
-                        });
 
-        for (int y = 0; y < gridHeight; ++y) {
-            for (int x = 0; x < gridWidth; ++x) {
-                for (int b = 0; b < NUM_BOXES_PER_BLOCK; ++b) {
-                    final int offset =
-                            (gridWidth * (NUM_BOXES_PER_BLOCK * (NUM_CLASSES + 5))) * y
-                                    + (NUM_BOXES_PER_BLOCK * (NUM_CLASSES + 5)) * x
-                                    + (NUM_CLASSES + 5) * b;
-
-                    final float xPos = (x + expit(output[offset + 0])) * blockSize;
-                    final float yPos = (y + expit(output[offset + 1])) * blockSize;
-
-                    final float w = (float) (Math.exp(output[offset + 2]) * ANCHORS[2 * b + 0]) * blockSize;
-                    final float h = (float) (Math.exp(output[offset + 3]) * ANCHORS[2 * b + 1]) * blockSize;
-
-                    final RectF rect =
-                            new RectF(
-                                    Math.max(0, xPos - w / 2),
-                                    Math.max(0, yPos - h / 2),
-                                    Math.min(bitmap.getWidth() - 1, xPos + w / 2),
-                                    Math.min(bitmap.getHeight() - 1, yPos + h / 2));
-                    final float confidence = expit(output[offset + 4]);
-
-                    int detectedClass = -1;
-                    float maxClass = 0;
-
-                    final float[] classes = new float[NUM_CLASSES];
-                    for (int c = 0; c < NUM_CLASSES; ++c) {
-                        classes[c] = output[offset + 5 + c];
-                    }
-                    softmax(classes);
-
-                    for (int c = 0; c < NUM_CLASSES; ++c) {
-                        if (classes[c] > maxClass) {
-                            detectedClass = c;
-                            maxClass = classes[c];
-                        }
-                    }
-
-                    final float confidenceInClass = maxClass * confidence;
-                    if (confidenceInClass > 0.01) {
-                        pq.add(new Recognition("" + offset, LABELS[detectedClass], confidenceInClass, rect));
-                    }
-                }
-            }
-        }
-
-        final ArrayList<Recognition> recognitions = new ArrayList<>();
-        for (int i = 0; i < Math.min(pq.size(), MAX_RESULTS); ++i) {
-            recognitions.add(pq.poll());
-        }
-        // "recognizeImage"
-
-        return recognitions;
+        return null;
     }
 
     @Override
