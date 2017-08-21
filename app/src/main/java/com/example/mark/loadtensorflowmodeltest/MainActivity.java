c3011d6011387ae0cd57676f315c942c6fd18a82
Mark <mazhenjin@sina.com>
2017-08-21 16:37:11 +0800

    突破 正则化理解和bitmap正则化

--- app/src/main/java/com/example/mark/loadtensorflowmodeltest/MainActivity.java
+++ app/src/main/java/com/example/mark/loadtensorflowmodeltest/MainActivity.java
@@ -23,41 +23,18 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPreferredConfig = Bitmap.Config.ALPHA_8;
-        //options.inPremultiplied = false;
-
-
-        //http://blog.csdn.net/haozipi/article/details/47183543?ref=myread
-        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.a0, options);
-
-        int x = bitmap.getWidth();
-        int y = bitmap.getHeight();
-        int[] intArray = new int[x * y];
-        bitmap.getPixels(intArray, 0, x, 0, 0, x, y);
-
-        //     0.    0.    0.    0.    0.    0.    0.    0.    0.    0.
-        //     0.    0.    0.    0.    0.    0.    0.    0.    0.    0.
-        //     2.    0. 185. 192.  0.    2.    0.    0.
-
-
-        //19 = 0
-        //20 = 33686018
-        //21 = 0
-        //22 = -1179010631
-        //23 = -1061109568
-        //24 = 0
-        //25 = 33686018
-        //26 = 0
-        //27 = 0
-
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 long start = System.currentTimeMillis();
 
                 try {
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inPreferredConfig = Bitmap.Config.ALPHA_8;
+
+                    final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.b0, options);
                     String charString = mDetector.decodeBitmap(bitmap);
+
                     mTextView.setText(String.format("解析用时间:(%d)毫秒, 字符为:(%s)", (System.currentTimeMillis()-start), charString));
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -68,7 +45,5 @@ public class MainActivity extends AppCompatActivity {
 
         mDetector = TensorFlowYoloDetector.create(getAssets(), NOTMNIST_MODEL_FILE, 28, "input", "out_softmax");
     }
-
-
 
 }
--- app/src/main/java/com/example/mark/loadtensorflowmodeltest/TensorFlowYoloDetector.java
+++ app/src/main/java/com/example/mark/loadtensorflowmodeltest/TensorFlowYoloDetector.java
@@ -7,7 +7,7 @@ import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 
 public class TensorFlowYoloDetector {
-    String[] labels = new String[]{
+    private String[] labels = new String[]{
             "A", "B", "C",
             "D", "E", "F",
             "G", "H", "I",
@@ -48,10 +48,7 @@ public class TensorFlowYoloDetector {
     public String decodeBitmap(final Bitmap bitmap) throws Exception{
         bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
 
-        for(int i = 0;i<intValues.length;i++){
-            int a = intValues[i];
-            floatValues[i] = a;
-        }
+        float[] floatValues = normalizedPixels(intValues, bitmap.getWidth(), bitmap.getHeight());
 
         inferenceInterface.feed(inputName, floatValues, 1, inputSize, inputSize, 1);
         inferenceInterface.run(outputNames, false);
@@ -63,12 +60,60 @@ public class TensorFlowYoloDetector {
         return labels[argmax(output)];
     }
 
-    private int argmax(float[] data){
-        for(int i = 0;i<data.length;i++){
-            if(data[i]==1)
-                return i;
+    private int argmax(float[] array){
+        int index = 0;
+        float largest = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > largest) {
+                largest = array[i];
+                index = i;
+            }
         }
-        return -1;
+        return index;
+    }
+
+
+    //     0.    0.    0.    0.    0.    0.    0.    0.    0.    0.
+    //     0.    0.    0.    0.    0.    0.    0.    0.    0.    0.
+    //     2.    0. 185. 192.  0.    2.    0.    0.
+
+
+    //19 = 0
+    //20 = 33686018
+    //21 = 0
+    //22 = -1179010631
+    //23 = -1061109568
+    //24 = 0
+    //25 = 33686018
+    //26 = 0
+    //27 = 0
+
+    /**
+     * 正则化数据，均值为0，标准差为0.5
+     * @param pixels 字节码像素
+     * @param width 宽度
+     * @param height 高度
+     * @return 正则后数据 -0.5~0.5
+     */
+    private float[] normalizedPixels(int[] pixels, int width, int height) {
+        float[] floatValues = new float[width * height];
+        for (int i = 0; i < pixels.length; ++i) {
+            floatValues[i] = byteToGary(pixels[i]);
+            floatValues[i] -= (255.0 / 2.0);
+            floatValues[i] /= 255.0;
+        }
+        return floatValues;
+    }
+
+
+    /**
+     * 灰度像素的转化为0-255数值
+     * @param pixels 灰度图像素
+     * @return 0~255
+     */
+    private int byteToGary(int pixels){
+        int pixelsElement = pixels & 0xff;
+        return pixelsElement;
     }
 
     //inferenceInterface.close();
